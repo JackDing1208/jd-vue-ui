e489ee8a698ccf7ca4defb20bc5a4577f6f66edb
JackDing <361164217@qq.com>
2019-04-30 17:51:40 +0800

    简单单元测试

--- src/main.js
+++ src/main.js
@@ -2,22 +2,42 @@ import Vue from 'vue'
 import Button from './Button'
 
 
-Vue.component('g-button',Button)
-
+Vue.component('g-button', Button)
 
 
 new Vue({
-    el:'#app',
-    data:{
-        message:'hello,world!'
+    el: '#app',
+    data: {
+        message: 'hello,world!'
     },
-    methods:{
-        alert(){
+    methods: {
+        alert() {
             alert('我被点了')
         }
     }
 })
 
+
+import chai from 'chai'
+const expect = chai.expect
+
+{   //测试icon位置
+    const Constructor = Vue.extend(Button)     //声明Vue组件构造器
+    const button = new Constructor({           //创建Vue组件实例并传入参数
+        propsData: {
+            icon: 'setting',
+            iconPosition: 'right'
+        },
+    })
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)  //获取CSS属性
+    expect(order).to.eq('1')
+    button.$el.remove()       //
+    button.$destroy()         //
+}
 
 
 
