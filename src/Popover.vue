b2d1c6de92d5d6fbbbec5d49fdaac65ccfec0850
JackDing <361164217@qq.com>
2019-05-11 23:50:51 +0800

    用阻止冒泡实现基本功能

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,31 +1,71 @@
 <template>
     <div class="pop">
-        <div class="content">
+        <div class="content" v-if="isOpen" ref="content">
             <slot name="content"></slot>
         </div>
-        <span class="button">
-                    <slot></slot>
+        <span class="button" @click.stop="toggle" ref="button">
+            <slot></slot>
         </span>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Popover"
+        name: "Popover",
+        data() {
+            return {
+                isOpen: false
+            }
+        },
+        methods: {
+            toggle() {
+                if (!this.isOpen) {             //轮子中不能阻止冒泡!!!
+                    this.open()
+                    this.$nextTick(
+                        document.addEventListener('click', this.documentListener)
+                    )
+                } else if(this.isOpen){
+                    this.close()
+                }
+            },
+            documentListener: function (e) {
+                if(!this.$refs.content.contains(e.target)){
+                    this.close()
+                }
+            },
+            open() {
+                this.isOpen = true
+                console.log(1);
+            },
+            close() {
+                this.isOpen = false
+                document.removeEventListener('click', this.documentListener)
+                console.log(2);
+            }
+        },
+        mounted() {
+
+        }
     }
 </script>
 
 <style scoped lang="scss">
-        .pop{
-            position: relative;
-            .content{
-                border: 1px solid red;
-                position: absolute;
-                transform: translateY(-100%);
+    .pop {
+        position: relative;
 
-            }
-            .button{
-                display: inline-block;
-            }
+        .content {
+            border: 1px solid red;
+            position: absolute;
+            transform: translateY(-100%);
+            max-width: 20em;
+            font-size: 14px;
+            padding: 0.5em 0.5em;
+            word-break: break-all;
+
         }
+
+        .button {
+            display: inline-block;
+        }
+    }
 </style>
