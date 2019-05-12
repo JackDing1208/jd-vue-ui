ef04ab1ea02b0a10d8ef8f8b0a5357611966a4cc
JackDing <361164217@qq.com>
2019-05-12 13:14:05 +0800

    添加hover触发事件

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="pop">
-        <div class="content" v-if="isOpen" ref="content">
+        <div class="content" v-if="isOpen" ref="content" :class="popPosition">
             <slot name="content"></slot>
         </div>
-        <span class="button" @click="toggle" ref="button">
+        <span class="button"  ref="button">
             <slot></slot>
         </span>
     </div>
@@ -17,10 +17,39 @@
                 isOpen: false
             }
         },
+        props: {
+            position: {
+                type: String,
+                default: 'top'
+            },
+            trigger:{
+                type: String,
+                default: 'click'
+            }
+        },
+        computed:{
+            popPosition(){
+                return `position-${this.position}`
+            }
+        },
+        mounted(){
+          if(this.trigger==='click'){
+              this.$refs.button.addEventListener('click',this.toggle)
+          }else if(this.trigger==='hover'){
+              this.$refs.button.addEventListener('mouseenter',this.open)
+              this.$refs.button.addEventListener('mouseleave',this.close)
+          }
+        },
         methods: {
             toggle() {
                 if (!this.isOpen) {             //轮子中不能阻止冒泡!!!
                     this.open()
+                    this.$nextTick(() => {    //处理渲染导致的异步
+                            document.body.appendChild(this.$refs.content)
+                            this.setPosition()
+                            document.addEventListener('click', this.documentListener)
+                        }
+                    )
                 } else if (this.isOpen) {
                     this.close()
                 }
@@ -32,22 +61,20 @@
             },
             open() {
                 this.isOpen = true
-                this.$nextTick(() => {    //处理渲染导致的异步
-                        document.body.appendChild(this.$refs.content)
-                        this.setPosition()
-                        document.addEventListener('click', this.documentListener)
-                    }
-                )
+
             },
             close() {
                 this.isOpen = false
-                document.removeEventListener('click', this.documentListener)
+                if(this.trigger==='click'){
+                    document.removeEventListener('click', this.documentListener)
+                    console.log(1);
+                }
             },
-            setPosition(){
-               let {width,height,top,left}=this.$refs.button.getBoundingClientRect()
+            setPosition() {
+                let {width, height, top, left} = this.$refs.button.getBoundingClientRect()
                 console.log(width, height, top, left);
-               this.$refs.content.style.left=left+scrollX+'px'
-               this.$refs.content.style.top=top+scrollY+'px'
+                this.$refs.content.style.left = left + scrollX + 'px'
+                this.$refs.content.style.top = top + scrollY + 'px'
 
             }
         },
@@ -55,19 +82,20 @@
 </script>
 
 <style scoped lang="scss">
-    $border-color:#333;
+    $border-color: #333;
     .pop {
         display: inline-block;
         vertical-align: middle;
+
         .button {
-            margin: 10px;
             display: inline-block;
         }
     }
+
     .content {
         border: 1px solid $border-color;
         border-radius: 4px;
-        filter: drop-shadow(1px 1px 3px rgba(0,0,0,0.5));
+        filter: drop-shadow(1px 1px 3px rgba(0, 0, 0, 0.5));
         background: white;
         display: inline-block;
         position: absolute;
@@ -77,19 +105,21 @@
         padding: 0.5em 0.5em;
         word-break: break-all;
         margin: -10px 0;
-        &::before{
+
+        &::before {
             content: '';
-            border:10px solid transparent;
+            border: 10px solid transparent;
             border-top-color: $border-color;
             position: absolute;
-            top:100%;
+            top: 100%;
         }
-        &::after{
+
+        &::after {
             content: '';
-            border:10px solid transparent;
+            border: 10px solid transparent;
             border-top-color: white;
             position: absolute;
-            top:calc(100% - 1px);
+            top: calc(100% - 1px);
         }
 
     }
